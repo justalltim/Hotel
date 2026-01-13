7e587102d7f868274b1b754179215fb908f64866
justalltim <natitoallan@gmail.com>
2026-01-13 09:35:14 +0100

    Refactor ModifierPrix method in Chambre.cpp
    Removed old ModifierPrix method and main function.

--- Chambre.cpp
+++ Chambre.cpp
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
+
 #include "Chambre.h"
 void Chambre::ModifierPrix(float nouveauPrix) {
     prix = nouveauPrix;
     std::cout << "Nouveau prix de la chambre : " << prix << std::endl;
-=======
-#include "Chambre.h"
-void Chambre::ModifierPrix() {
-    std::cout << "Nouveau prix: " << std::endl;
-    std::cin >> nouveauPrix;
-    prix = nouveauPrix;
-}
-int main() {
-    Chambre chambre1(101, "Single", 75);
-    chambre1.ModifierPrix();
->>>>>>> ccbb46c0f5e0f43fb3979032a848e07793dfbc9c
-}
