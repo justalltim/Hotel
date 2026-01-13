f9c50bd6de5f61d04a8c3181145d3670664c9045
justalltim <natitoallan@gmail.com>
2026-01-13 09:43:41 +0100

    Remove duplicate Reservation class definition

--- Reservation.h
+++ Reservation.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef RESERVATION_H
 #define RESERVATION_H
 #include <iostream>
@@ -30,26 +29,3 @@ public:
     int getNbreNuits() const { return NbreNuits; }
 };
 #endif
-=======
-#include <iostream>
-
-class Reservation {
-private:
-    int idClient;
-    int idHotel;
-    int idChambre;
-    Date dateDebut;
-    int MontantTotal;
-    int NbreNuits;
-public:
-    Reservation(int idClient, int idHotel, int idChambre, Date dateDebut        , int MontantTotal, int NbreNuits) {
-        this->idClient = idClient;
-        this->idHotel = idHotel;
-        this->idChambre = idChambre;
-        this->dateDebut = dateDebut;
-        this->MontantTotal = MontantTotal;
-        this->NbreNuits = NbreNuits;
-    }      
-
-};
->>>>>>> ccbb46c0f5e0f43fb3979032a848e07793dfbc9c
