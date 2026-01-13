f82fe2e8d4910e47cd5cd7cde90db7868457ce7d
justalltim <natitoallan@gmail.com>
2026-01-13 09:45:06 +0100

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef CLIENT_H
 #define CLIENT_H
 #include <iostream>
@@ -20,23 +19,4 @@ public:
     int getIdentifiant() const { return Identifiant; }
 };
 #endif
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <iostream>
-class Client
-{
-private: 
-    std::string nom;
-    std::string prenom;
-    int Identifiant;
-public:
-    Client( std::string nom, std::string prenom, int Identifiant) {
-        this->nom = nom;
-        this->prenom = prenom;
-        this->Identifiant = Identifiant;
-    }  
-             
-};
->>>>>>> ccbb46c0f5e0f43fb3979032a848e07793dfbc9c
+
